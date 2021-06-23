72ed84ce88f02621b13dd2fac16e920149f92ed3
OldScripter <Oldscripter@gmail.com>
2021-06-24 00:11:00 +0300

    Picture painter is created

--- main.cpp
+++ main.cpp
@@ -1,6 +1,59 @@
 #include <iostream>
+#include <fstream>
+#include <random>
+
+/*
+ * Safe getting of integer from keyboard.
+ */
+int getIntFromKeyboard()
+{
+    do
+    {
+        std::string input;
+        std::getline(std::cin, input);
+        try
+        {
+            return std::stoi(input);
+        }
+        catch (std::exception& e)
+        {
+            std::cerr << "Bad input. Try again.\n";
+        }
+    } while (true);
+
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    const std::string OUTPUT_PATH = "..\\pic.txt";
+    int width;
+    int height;
+    srand(time(nullptr));
+
+    std::cout << "Please enter the width: ";
+    width = getIntFromKeyboard();
+    std::cout << "You've entered: " << width << "\n";
+    std::cout << "Please enter the height: ";
+    height = getIntFromKeyboard();
+    std::cout << "You've entered: " << height << "\n";
+
+    std::ofstream outputStream(OUTPUT_PATH, std::ios::binary);
+    if (outputStream.is_open())
+    {
+        for (int h = 0; h < height; h++)
+        {
+            for (int w = 0; w < width; w++)
+            {
+                int value = rand() % 2;
+                outputStream << value;
+            }
+            outputStream <<'\n';
+        }
+        outputStream.close();
+    }
+    else
+    {
+        std::cerr << "File read error! Check path or rights for file edit.\n";
+    }
+
     return 0;
 }
